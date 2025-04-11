af4bb4e844ce30e2cb1595c1aeb039963dcc3855
Isam Abdullah Balghari <isamabdullah88@gmail.com>
2025-04-11 16:51:44 +0500

    Used base class

--- experiments/Marangoni.py
+++ experiments/Marangoni.py
@@ -9,102 +9,22 @@ from itertools import groupby
 from math import floor
 import cv2
 import numpy as np
+from tqdm import tqdm
 
+from .Experiment import Experiment
 from media import VideoReader
 from filters import Smoothen
 
-class Marangoni():
+class Marangoni(Experiment):
     def __init__(self):
-        self._vidreader = None
-        self.frame_width = None
-        self.frame_height = None
-        self.frame_count = 0
-        
-        self.active_duration = []
-        self.tracked_pts = []
+        super().__init__()
 
         # self.model = StarDist2D.from_pretrained("2D_versatile_fluo")
 
-    def add_video(self, video_path):
-        self._vidreader = VideoReader(video_path)
-        self.frame_width = self._vidreader.width
-        self.frame_height = self._vidreader.height
-        self.frame_count = self._vidreader.frame_count
+    def track(self):
+
         self._vidreader.seek(250)
 
-
-    def frame(self, index=None):
-        if (index is None) or (self.active_duration is None):
-            f = self._vidreader.read()
-        else:
-            f = self._vidreader.read(self.active_duration[index])
-
-        return f
-
-    def crop_intime(self):
-        """
-        Finds the duration where the experiment happens.
-        """
-        self.bg_subtractor = cv2.createBackgroundSubtractorMOG2(history=500, varThreshold=16,
-                                                                detectShadows=True)
-        
-        motion_scores = []
-
-        for i in  range(self.frame_count):
-            frame = self._vidreader.read()
-
-            mask = self.bg_subtractor.apply(frame)
-
-            if i == 0:
-                motion_scores.append(0)
-                continue
-
-            score = np.sum(mask) / 255
-            motion_scores.append(score)
-
-        motion_scores = np.array(motion_scores)
-        motion_scores = motion_scores / np.max(motion_scores)
-        
-        win_len = 15
-
-        scores_bin = []
-        for i in range(1, self.frame_count+1):
-            idx = i-win_len
-            if idx < 0: idx = 0
-
-            score = motion_scores[idx:i]
-
-            if np.mean(score) > 0.4:
-                scores_bin.append(1)
-            else:
-                scores_bin.append(0)
-
-        self._vidreader.seek(0)
-    
-        groups = []
-        idx = 0
-        for _, group_ in groupby(scores_bin):
-            group = list(group_)
-            if sum(group) < 0.8*len(group):
-                idx += len(group)
-                continue
-            groups.append((idx, len(group)+idx))
-            idx += len(group)
-
-        groups = sorted(groups, key=lambda x: x[1]-x[0], reverse=True)
-        
-        start, end = groups[0]
-        
-        start = max(start-20, 0)
-        end = min(end+20, self._vidreader.frame_count)
-
-        self.active_duration = list(range(start, end, 1))
-
-        self.frame_count = len(self.active_duration)
-
-
-    def track(self):
-        
         import os
 
         tempdir =  "./tempdir"
@@ -122,7 +42,7 @@ class Marangoni():
 
         self._vidreader.seek(200)
 
-        for i in range(480):
+        for i in tqdm(range(480), desc="Marangoni", total=480):
 
             frame = self._vidreader.read()
             frame = frame[:, 200:1750]
@@ -169,12 +89,12 @@ class Marangoni():
             #     r = int(r)
 
                 # Draw the circle
-            cv2.circle(frame, (x, y), r, (255, 0, 0), 3)  # Green circle
+            cv2.circle(frame, (x, y), r, (0, 0, 255), 2)  # Green circle
 
             videowriter.write(frame)
 
-            if i%10==0:
-                print('Processed: ', i)
+            # if i%10==0:
+            #     print('Processed: ', i)
 
         # cv2.destroyAllWindows()
         videowriter.release()
