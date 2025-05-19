b5f63d5fc4d3e553125eacaa6d3f1dd6c2b2db0b
Isam Abdullah Balghari <isamabdullah88@gmail.com>
2025-05-19 11:17:08 +0500

    frame-crop moved

--- gui/Core.py
+++ gui/Core.py
@@ -1,14 +1,7 @@
+from math import floor, ceil
 import cv2
 import numpy as np
 from PIL import Image, ImageDraw
-
-class Circle:
-    """Basic circle unit class."""
-    def __init__(self, **kwargs):
-        self.cx = kwargs.get('cx', 0)
-        self.cy = kwargs.get('cy', 0)
-        self.rad = kwargs.get('rad', 0)
-
 
 def circilize(width, height):
     """Create a fully transparent image (RGBA)"""
@@ -34,3 +27,57 @@ def circilize(width, height):
     draw.ellipse(circle_bounds, fill=circle_color)
 
     return np.array(image), matte
+
+
+
+def fcrop_coords(frame, center, x):
+    """
+    Generates dynamic circle based on user's position x and center. Handles out of view cases.
+    """
+    cx, cy = center
+    ex, ey = x
+
+    radx = 2*abs(ex - cx)
+    rady = 2*abs(ey - cy)
+    
+    circ, matte = circilize(radx, rady)
+    
+    height, width = frame.shape[:2]
+    
+    frame = frame.copy()
+
+    cysrt = cy-floor(rady/2)
+    cyend = cy+ceil(rady/2)
+    cxsrt = cx-floor(radx/2)
+    cxend = cx+ceil(radx/2)
+
+    if cxsrt < 0:
+        circ = circ[:,-cxsrt:]
+        matte = matte[:,-cxsrt:]
+        cxsrt = 0
+
+    if cxend > width:
+        cxend = width
+        circ = circ[:,:cxend-cxsrt]
+        matte = matte[:,:cxend-cxsrt]
+
+    if cysrt < 0:
+        circ = circ[-cysrt:,:]
+        matte = matte[-cysrt:,:]
+        cysrt = 0
+
+    if cyend > height:
+        cyend = height
+        circ = circ[:cyend-cysrt, :radx]
+        matte = matte[:cyend-cysrt, :radx]
+
+    frame_crop = frame[cysrt:cyend, cxsrt:cxend]
+
+    frame_cropbd = cv2.addWeighted(frame_crop, 0.6, circ, 0.4, 0)
+    frame_cropbd[matte < 150] = frame_crop[matte < 150]
+    
+    matte_frame = np.zeros((height, width), np.uint8)
+    matte_frame[cysrt:cyend, cxsrt:cxend] = matte
+    frame[cysrt:cyend, cxsrt:cxend] = frame_cropbd
+
+    return frame, matte_frame
