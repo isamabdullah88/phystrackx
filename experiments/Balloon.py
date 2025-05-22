e469d2747fac34f586099eea750b1999d8c38876
Isam Abdullah Balghari <isamabdullah88@gmail.com>
2025-05-22 16:43:20 +0500

    cleanup and midst of restructuring

--- experiments/Balloon.py
+++ experiments/Balloon.py
@@ -1,4 +1,4 @@
-
+from math import floor
 import cv2
 import numpy as np
 from tqdm import tqdm
@@ -18,60 +18,39 @@ class Balloon(Experiment):
         super().__init__()
 
         self._trackpath = trackpath
-        # self.model = StarDist2D.from_pretrained("2D_versatile_fluo")
 
-    def preprocess(self, cframe, i):
-        """Preprocesses the cropped frame in multiple steps to reliably detect balloon boundary"""
-        framep = cframe.copy()
+    def resize(self):
+        """Resize frame shape to lower"""
+        if self.frame_height <= 360:
+            return self.frame_width, self.frame_height
+        
+        self.aspratio = self.frame_width/self.frame_height
 
-        gray = cv2.cvtColor(framep, cv2.COLOR_BGR2GRAY)
-        cv2.imwrite(f"gray-plain-{i}.png", gray)
+        self.frame_height = 360
+        self.frame_width = floor(self.aspratio * self.frame_height)
+
+        return self.frame_width, self.frame_height
+
+
+    def preprocess(self, frame):
+        """Preprocesses the frame to sharpen the edges"""
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
         clahe = cv2.createCLAHE(clipLimit=4.0, tileGridSize=(4,4))
         gray = clahe.apply(gray)
-        cv2.imwrite(f"gray-enhanced-{i}.png", gray)
-        edges = cv2.Canny(gray, 50, 150)
-        cv2.imwrite(f"edges-enhanced-{i}.png", edges)
-        # edgesp = edges.copy()*0
-        # edgesp[300:750, 420:800] = edges[300:750, 420:800]
-
-        # Find contours of the moving object (the balloon)
-        contours, _ = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        # contours = sorted(contours, key=cv2.contourArea, reverse=True)
-        
-        cv2.drawContours(gray, contours, -1, (0, 0, 0), thickness=5)
-        cv2.imwrite(f"gray-contour-{i}.png", gray)
 
         return gray
     
+    def prepmask(self, mask):
+        """Preprocess and convert user defined mask to ellipse. Also convert from opencv
+        (x,y) to (r,c)"""
+        _, thresh = cv2.threshold(mask, 1, 255, cv2.THRESH_BINARY)
+        contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+        contour = max(contours, key=cv2.contourArea)
+        ellipse = cv2.fitEllipse(contour)
+        (xc, yc), (a, b), angle = ellipse
 
-    def fit_ransac(self, contours, xoff, yoff):
-        """Fits ellipse on data while using ransac to reject outliers"""
-        data = np.vstack([cont.reshape(-1, 2) for cont in contours])
-        
-        model, inliers = ransac(data, EllipseModel, min_samples=300,
-                                    residual_threshold=25, max_trials=10)
-
-        if model is None:
-            print("RANSAC failed to fit ellipse.")
-            return None
-        # else:
-        # print("Ellipse parameters:", model.params)
-        # print('inliers: ', inliers)
-            # # Draw fitted ellipse
-        xc, yc, a, b, theta = model.params
-        ellipse = plt.matplotlib.patches.Ellipse((xc, yc), 2*a, 2*b, angle=np.rad2deg(theta),
-                                                edgecolor='red', facecolor='none', linewidth=2, label='RANSAC Ellipse')
-        fig, ax = plt.subplots(figsize=(8, 6))
-        ax.add_patch(ellipse)
-        ax.plot(data[:,0], data[:,1], '.')
-
-        ax.set_aspect('equal')
-        # ax.set_xlim(0, 400)
-        # ax.set_ylim(0, 300)
-        ax.legend()
-        plt.title("Ellipse Fitting using RANSAC")
-        plt.show()
-        return xc + xoff, yc+yoff, a, b, theta
+        return (yc, xc), (b/2, a/2), angle
 
 
     def track(self, mask, startidx=0, endidx=0):
@@ -87,8 +66,8 @@ class Balloon(Experiment):
         Note: Make sure that the first frame has almost perfectly accurate detection.
         """
         fourcc = cv2.VideoWriter_fourcc(*"mp4v")
-        # self._videowriter = cv2.VideoWriter(self._trackpath, fourcc, 24, (self.frame_width, self.frame_height))
-        self._videowriter = cv2.VideoWriter(self._trackpath, fourcc, 24, (640, 360))
+        self._videowriter = cv2.VideoWriter(self._trackpath, fourcc, 24, (self.frame_width, self.frame_height))
+        # self._videowriter = cv2.VideoWriter(self._trackpath, fourcc, 24, (640, 360))
 
         self._vidreader.seek(startidx)
         
@@ -101,19 +80,14 @@ class Balloon(Experiment):
         # smoothenx = Smoothen(tol=50)
         # smootheny = Smoothen(tol=50)
         # smoothenr = Smoothen(tol=100)
-        # mask = np.zeros((self.frame_height, self.frame_width))
-        # mask[300:700, 420:800] = 255
 
-        # mask = cv2.resize(mask, (self.frame_width, self.frame_height))
         mask = cv2.imread("mask-balloon.png", 0)
-        mask = cv2.resize(mask, (640, 360))
-        points = cv2.findNonZero(mask)
-
-        # Get bounding rectangle
-        x, y, w, h = cv2.boundingRect(points)
+        mask = cv2.resize(mask, (self.frame_width, self.frame_height))
+        
+        ellipse = self.prepmask(mask)
 
         frame = self._vidreader.read()
-        frame = cv2.resize(frame, (640, 360))
+        frame = cv2.resize(frame, (self.frame_width, self.frame_height))
 
         # RANSAC based ellipse fitting -----------------
         """
@@ -122,32 +96,13 @@ class Balloon(Experiment):
         contours = sorted(contours, key=cv2.contourArea, reverse=True)[:5]
         ellipse = self.fit_ransac(contours, x, y)
         """
-
-        # Active contour fitting ------------------------
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        clahe = cv2.createCLAHE(clipLimit=4.0, tileGridSize=(4,4))
-        gray = clahe.apply(gray)
-        # smoothed = gaussian(gray, 3)
-
-        _, thresh = cv2.threshold(mask, 1, 255, cv2.THRESH_BINARY)
-        contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-        contour = max(contours, key=cv2.contourArea)
-        ellipse = cv2.fitEllipse(contour)
-        cv2.ellipse(frame, ellipse, (0, 255, 0), 1)
-
-        print('ellipse: ', ellipse)
-
-        # Show
-        # plt.imshow(cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY), cmap="gray")
-        # plt.title("Fitted Ellipse")
-        # plt.axis('off')
-        # plt.show()
-        # exit()
-        # initpts = contour.reshape(-1, 2)[:, [1, 0]]
-        initpts = ptsellpise(ellipse)[:, [1,0]]
+        
+        initpts = ptsellpise(ellipse)
 
         # Run active contour on this frame
         snake = active_contour(gray, initpts)
+
+        # Fit ellipse on the data
         snakecont = snake.copy().astype(np.float32).reshape(-1, 1, 2)
         ellipse = cv2.fitEllipse(snakecont[:,:,[1, 0]])
         # cv2.ellipse(frame, balloon, (0, 255, 0), 2)
@@ -170,7 +125,7 @@ class Balloon(Experiment):
         for i in tqdm(range(fcount-1), desc="Balloon", total=fcount):
 
             frame = self._vidreader.read()
-            frame = cv2.resize(frame, (640, 360))
+            frame = cv2.resize(frame, (self.frame_width, self.frame_height))
             gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
             clahe = cv2.createCLAHE(clipLimit=4.0, tileGridSize=(4,4))
             gray = clahe.apply(gray)
