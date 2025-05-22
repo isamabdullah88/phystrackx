b3635b184004447e9a593c6d8b5e6bc91a901dd4
Isam Balghari <isamabdullah88@gmail.com>
2025-05-22 20:50:56 +0500

    Utils added

---
+++ experiments/Utils.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+def ptsellpise(ellipse, num_points=100):
+    (cx, cy), (a, b), angle = ellipse
+    theta = np.radians(angle)
+    t = np.linspace(0, 2 * np.pi, num_points)
+    
+    x = cx + a/2 * np.cos(t) * np.cos(theta) - b/2 * np.sin(t) * np.sin(theta)
+    y = cy + a/2 * np.cos(t) * np.sin(theta) + b/2 * np.sin(t) * np.cos(theta)
+
+    return np.stack((y, x), axis=1)  # shape (N, 2)
