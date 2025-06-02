e9758abbcc6265531b6cb309dd7f57b9db73aff3
Isam Balghari <isamabdullah88@gmail.com>
2025-06-02 20:18:21 +0500

    Collision updated with working config. Multiple objects supported.

--- experiments/Collision.py
+++ experiments/Collision.py
@@ -1,201 +1,100 @@
+from tqdm import tqdm
 import cv2
 import numpy as np
+from math import floor
 import matplotlib.pyplot as plt
-from itertools import groupby
-from media import VideoReader
+from .Experiment import Experiment
+from core.Rect import NormalizedRect, PixelRect
 
-class Collision():
-    def __init__(self):
-        self._vidreader = None
-        self.fwidth = None
-        self.fheight = None
-        self.frame_count = 0
-        # self._clip_start = 0
+class Collision(Experiment):
+    def __init__(self, trackpath):
+        super().__init__()
         
-        self.active_duration = []
-        self.tracked_pts = []
-
-    def add_video(self, video_path):
-        self._vidreader = VideoReader(video_path)
-        self.fwidth = self._vidreader.width
-        self.fheight = self._vidreader.height
-        self.frame_count = self._vidreader.frame_count
-
-
-    def frame(self, index=None):
-        if (index is None) or (self.active_duration is None):
-            f = self._vidreader.read()
-        else:
-            f = self._vidreader.read(self.active_duration[index])
-
-        return f
-
-    def crop_intime(self):
-        """
-        Finds the duration where the experiment happens.
-        """
-        self.bg_subtractor = cv2.createBackgroundSubtractorMOG2(history=500, varThreshold=16,
-                                                                detectShadows=True)
+        self._trackpath = trackpath
+        self.trackpts = []
         
-        motion_scores = []
-
-        for i in  range(self.frame_count):
-            frame = self._vidreader.read()
-
-            mask = self.bg_subtractor.apply(frame)
-
-            if i == 0:
-                motion_scores.append(0)
-                continue
-
-            score = np.sum(mask) / 255
-            motion_scores.append(score)
-
-        motion_scores = np.array(motion_scores)
-        motion_scores = motion_scores / np.max(motion_scores)
-        
-        win_len = 15
-
-        scores_bin = []
-        for i in range(1, self.frame_count+1):
-            idx = i-win_len
-            if idx < 0: idx = 0
-
-            score = motion_scores[idx:i]
-
-            if np.mean(score) > 0.4:
-                scores_bin.append(1)
-            else:
-                scores_bin.append(0)
-
-        self._vidreader.seek(0)
     
-        groups = []
-        idx = 0
-        for _, group_ in groupby(scores_bin):
-            group = list(group_)
-            # The group has to be valid group having 80% 1's atleast
-            # print('sum: ', sum(group))
-            # print('group: ', group)
-            if sum(group) < 0.8*len(group):
-                idx += len(group)
-                continue
-            groups.append((idx, len(group)+idx))
-            idx += len(group)
-
-        groups = sorted(groups, key=lambda x: x[1]-x[0], reverse=True)
-        # print(g)
-        print(groups)
-        start, end = groups[0]
+    def resize(self):
+        """Resize frame shape to lower"""
+        if self.fheight <= 360:
+            return self.fwidth, self.fheight
         
-        start = max(start-20, 0)
-        end = min(end+20, self._vidreader.frame_count)
+        self.aspratio = self.fwidth/self.fheight
 
-        self.active_duration = list(range(start, end, 1))
-        # self._vidreader.seek(max_group[0])
-        # self._vidreader.frame_count = max_group[1] - max_group[0]
-        # self._vidreader.set_extents(start, end-start)
-
-        # self._clip_start = start
-        # self.frame_count = self._vidreader.frame_count
-        # print('start: ', self._clip_start)
-        # print('frame count: ', self.frame_count)
-        self.frame_count = len(self.active_duration)
-
-        # plt.plot(scores_bin)
-        # plt.plot(motion_scores)
-        # plt.show()
-
-
-    def track(self, points):
+        self.fheight = 360
+        self.fwidth = floor(self.aspratio * self.fheight)
+    
+    
+    def track(self, rects:NormalizedRect, startidx=0, endidx=0):
+        
+        # Tracking
+        fourcc = cv2.VideoWriter_fourcc(*"mp4v")
+        # self.resize()
+        self._videowriter = cv2.VideoWriter(self._trackpath, fourcc, 24, (self.fwidth, self.fheight))
+        
+        if endidx == 0:
+            fcount = self._vidreader.fcount - startidx
+        else:
+            fcount = endidx - startidx
+        print('fcount: ', fcount)
 
         # Define Lucas-Kanade optical flow parameters
-        lk_params = dict(
-            winSize=(15, 15),      # Size of search window for each pyramid level
-            maxLevel=2,            # Number of pyramid levels (0 means single level)
-            criteria=(             # Termination criteria for iterative algorithm
-                cv2.TERM_CRITERIA_EPS |      # Stop if accuracy is reached
-                cv2.TERM_CRITERIA_COUNT,     # Stop if max iterations reached
-                10,                          # Maximum iterations
-                0.03                         # Minimum accuracy/epsilon
-            )
-        )
+        lk_params = dict(winSize=(15, 15), maxLevel=5,criteria=(cv2.TERM_CRITERIA_EPS | \
+            cv2.TERM_CRITERIA_COUNT, 10, 0.03))
+        
+        ptstrack = []
+        
+        self._vidreader.seek(startidx)
+        
+        for rect in rects:
+            rect = rect.norm2pix(self.fwidth, self.fheight)
+            
+            frame = self._vidreader.read()
+            fgray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            
+            mask = np.zeros_like(fgray, dtype=np.uint8)
+            mask[rect.ymin:rect.ymax, rect.xmin:rect.xmax] = 255
+            
+            p0 = cv2.goodFeaturesToTrack(fgray, maxCorners=5, qualityLevel=0.5, minDistance=10, blockSize=5, mask=mask)
+            p0 = np.array(p0, dtype=np.float32).reshape(-1, 1, 2)
+            
+            ptstrack.append(p0)
+            
+        fprev = None
+        for i in tqdm(range(1, fcount-1), desc="Sliding Friction", total=fcount):
+            frame = self._vidreader.read()
+            fgray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-
-        for point in points:
-            p0 = np.array(point, dtype=np.float32).reshape(-1, 1, 2)
-            # Convert first frame to grayscale if necessary
-            # OpenCV optical flow requires grayscale images
-            # if self.processor.frames[0].ndim == 3 and self.processor.frames[0].shape[2] == 3:
-            #     old_gray = cv2.cvtColor(self.processor.frames[0], cv2.COLOR_BGR2GRAY)
-            # else:
-            #     old_gray = self.processor.frames[0]  # Frame is already grayscale
-
-            # Initialize dictionary to store tracking history
-            # Keys: point indices, Values: lists of (x,y) coordinates
-            # points_tracked = {i: [p] for i, p in enumerate(points)}
-            tracked_pts = np.zeros((2, self.frame_count))
-
-            # Process all frames after the first one
-            fprev = None
-            # self._vidreader.seek(self._clip_start)
-            for i in range(self.frame_count):
-                # Convert current frame to grayscale if necessary
-                # if frame.ndim == 3 and frame.shape[2] == 3:
-                #     frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-                # else:
-                #     frame_gray = frame
-                frame = self._vidreader.read()
-                fgray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-                # Calculate optical flow using Lucas-Kanade method
-                # p1: calculated new positions of input points
-                # st: status array (1 = point found, 0 = point lost)
-                # err: array of error measures for each point
-                if fprev is None:
-                    fprev = fgray.copy()
-                
-                # cv2.imwrite(f"gray-{i:03d}.png", fgray)
-
-                p1, st, err = cv2.calcOpticalFlowPyrLK(fprev, fgray, p0, None, **lk_params)
-                # print('p1: ', type(p1))
-                # print('p1: ', p1)
-                # print('err: ', err)
-
-                tracked_pts[:, i] = p1.reshape(2,)
-                # Select good points by checking status array
-                # good_new = p1[st == 1]  # New positions of successfully tracked points
-                # good_old = p0[st == 1]  # Previous positions of successfully tracked points
-
-                # Update tracking history for successfully tracked points
-                # for i, (new, old) in enumerate(zip(good_new, good_old)):
-                #     a, b = new.ravel()  # Extract x,y coordinates from array
-                #     points_tracked[i].append((a, b))  # Add new position to tracking history
-
-                # Update for next iteration
-                # old_gray = frame_gray.copy()
-                # p0 = good_new.reshape(-1, 1, 2)  # Update previous points
+            if fprev is None:
                 fprev = fgray.copy()
-                p0 = p1
+                continue
+
+            for i,p0 in enumerate(ptstrack):
+                p1, st, err = cv2.calcOpticalFlowPyrLK(fprev, fgray, p0, None, **lk_params)
+                x, y = pts2pt(p1)
+                cv2.circle(frame, (x, y), radius=5, color=(0,255,0), thickness=2)
+                ptstrack[i] = p1
+            
+            # plt.imshow(frame)
+            # plt.plot(p1[:,:,0], p1[:,:,1], 'r.')
+            # plt.show()
+            fprev = fgray.copy()
+            # p0 = p1
+            
+            self._videowriter.write(frame)
+            
+        self._videowriter.release()
 
             # Add the set of tracked points
-            self.tracked_pts.append(tracked_pts)
+            # self.tracked_pts.append(tracked_pts)
 
-        # Visualize tracked points on all frames
-        # for i, frame in enumerate(self.processor.frames):
-        #     for j, point in points_tracked.items():
-        #         if i < len(point):  # Check if point was tracked in this frame
-        #             x, y = point[i]
-        #             # Draw green circle at tracked point position
-        #             cv2.circle(frame, (int(x), int(y)), 3, (0, 255, 0), -1)
 
-        # Store tracked points for later analysis
-        # self.processor.points_tracked = points_tracked
-
+def pts2pt(pts):
+    """Convert points to single mean point."""
+    x, y = np.mean(np.squeeze(pts), axis=0)
+    return floor(x), floor(y)
 
 
 if __name__ == '__main__':
-    sliding_friction = Collision()
-    sliding_friction.add_video("/home/isam/MSPhysics/Projects/PhysTrack/phystrackx/HeadOn.MP4")
-
-    sliding_friction.crop_intime()
+    collision = Collision()
+    collision.add_video("HeadOn.MP4")
--- gui/CollisionApp.py
+++ gui/CollisionApp.py
@@ -1,56 +1,77 @@
 
+import os
 import cv2
-# import tkinter as tk
+import threading
 import customtkinter as ctk
 from PIL import Image, ImageTk
 from matplotlib import pyplot as plt
 from tkinter import messagebox
-# from video_processing import VideoProcessor
 from math import floor
 
 from .App import App
 from experiments.Collision import Collision
+from core.Rect import PixelRect
+from .components.Spinner import SpinnerPopup
+from .components.Seekbar import CutSeekBar
 
 class CollisionApp(App):
     def __init__(self, root):
         super().__init__(root)
-        # self.processor = VideoProcessor()
-        self.collision = Collision()
+        
+        sfimg = Image.open("assets/rectanglebd.png").resize((80, 80), Image.Resampling.LANCZOS)
+        sfimg = ImageTk.PhotoImage(sfimg)
+        self.rectbd = ctk.CTkButton(self.toolbarf, text="", width=80, height=80,
+                                      image=sfimg, command=self.drawrect)
+        self.rectbd.pack(pady=10)
+        
+        self.seekbar = CutSeekBar(self.video_frame, ondrag=self.updateframe)
+        
+        self._rcoords = None
+        self._rects = []
+        
+        tempdir = './temp'
+        if not os.path.exists(tempdir):
+            os.makedirs(tempdir)
+        self._trackpath = os.path.join(tempdir, 'track-collision.mp4')
+        self.collision = Collision(trackpath=self._trackpath)
+        
 
     def load_video(self, videopath):
         self.collision.add_video(videopath)
-        self.collision.crop_intime()
+        # self.collision.crop_intime()
+        
+        self.seekbar.pack(pady=10)
+        self.seekbar.setcount(self.collision.fcount)
 
         frame1 = self.collision.frame(0)
-        self.display_first_frame(frame1)
+        self.dispframe(frame1)
 
-    def display_first_frame(self, frame=None):
+    def dispframe(self, frame=None):
+        fwidth = self.collision.fwidth
+        fheight = self.collision.fheight
+        frame = self.resize_frame(frame, fwidth, fheight)
+        self.fheight, self.fwidth = frame.shape[:2]
+        
         img = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
-        photo = ImageTk.PhotoImage(image=img)
+        self.photo = ImageTk.PhotoImage(image=img)
 
-        self.frame_ox = floor(self.cwidth/2 - self.collision.fwidth/2)
-        self.frame_oy = floor(self.cheight/2 - self.collision.fheight/2)
+        self.fx = floor(self.cwidth/2 - self.collision.fwidth/2)
+        self.fy = floor(self.cheight/2 - self.collision.fheight/2)
 
-        print('frame ox: ', self.frame_ox)
-        self.videoview.create_image(self.frame_ox, self.frame_oy, image=photo, anchor='nw')
-        self.videoview.photo = photo
+        self.imgview = self.videoview.create_image(self.fx, self.fy, image=self.photo, anchor='nw')
 
-        self.slider.configure(from_=0, to=self.collision.frame_count - 1)
-        self.slider.set(0)
+    def updateframe(self):
+        """Updates the frame displayed in the video view based on the slider position."""
 
-    def update_frame(self, event):
-        frame_idx = int(self.slider.get())
+        frame = self.collision.frame(index=self.seekbar.idx)
+        fwidth = self.collision.fwidth
+        fheight = self.collision.fheight
+        frame = self.resize_frame(frame, fwidth, fheight)
 
-        frame = self.collision.frame(index=frame_idx)
+        img = Image.fromarray(cv2.cvtColor(frame.copy(), cv2.COLOR_BGR2RGB))
+        self.photo = ImageTk.PhotoImage(image=img)
 
-        img = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
-        photo = ImageTk.PhotoImage(image=img)
-
-        x = floor(self.cwidth/2 - self.collision.fwidth/2)
-        y = floor(self.cheight/2 - self.collision.fheight/2)
-        # self.videoview.delete('all')
-        self.videoview.create_image(x, y, image=photo, anchor='nw')
-        self.videoview.photo = photo
+        self.videoview.itemconfig(self.imgview, image=self.photo)
 
     def mark_axes(self):
 
@@ -66,15 +87,49 @@ class CollisionApp(App):
         def store_click(event):
             """ Store the clicked coordinates and draw a point. """
             x, y = event.x, event.y
-            self._ref_frame = [x-self.frame_ox, y-self.frame_oy]  # Store coordinates
+            # self._ref_frame = [x-self.frame_ox, y-self.frame_oy]  # Store coordinates
             self.videoview.create_oval(x-3, y-3, x+3, y+3, fill="red", outline="black")  # Draw a small dot
 
-            print(self._ref_frame)
+            # print(self._ref_frame)
             self.videoview.unbind("<Motion>")
             self.videoview.unbind("<Button>")
 
         self.videoview.bind("<Motion>", update_axes)
         self.videoview.bind("<Button>", store_click)
+
+    def drawrect(self):
+        """Draws rectangle with simple lines"""
+        self._ctkbox = None
+        
+        def ondown(event):
+            # if self._ctkbox is not None:
+            #     self.videoview.delete(self._ctkbox)
+            self._ctbox = None
+            
+            self._rcoords = (event.x, event.y)
+            
+            self._ctkbox = self.videoview.create_rectangle(event.x, event.y, event.x, event.y, outline="red")
+            
+        def inrect(event):
+            sx, sy = self._rcoords
+            ex, ey = (event.x, event.y)
+
+            # print('Rect Orig: ', (sx, sy, ex, ey))
+
+            self.videoview.coords(self._ctkbox, sx, sy, ex, ey)
+            
+        def onrelease(event):
+            sx, sy = self._rcoords
+            ex, ey = (event.x, event.y)
+
+            rect = PixelRect(sx-self.fx, sy-self.fy, ex-sx, ey-sy)
+            print('Rect tr: ', rect.totuple())
+            self._rects.append(rect.pix2norm(self.fwidth, self.fheight))
+            
+
+        self.videoview.bind("<Button-1>", ondown)
+        self.videoview.bind("<B1-Motion>", inrect)
+        self.videoview.bind("<ButtonRelease-1>", onrelease)
 
     def plot_distances(self):
         if len(self.collision.tracked_pts) < 1:
@@ -84,7 +139,7 @@ class CollisionApp(App):
         # ox, oy = self._ref_frame
 
         num_tracks = len(self.collision.tracked_pts)
-        _, axes = plt.subplots(num_tracks, 2, figsize=(6, 5))
+        _, axes = plt.subplots(num_tracks+1, 2, figsize=(6, 5))
 
         for i in range(num_tracks):
             tracked_pts = self.collision.tracked_pts[i]
@@ -101,14 +156,20 @@ class CollisionApp(App):
 
     def start_tracking(self):
         """
-        Implements Lucas-Kanade optical flow tracking for marked points across video frames.
-        This method processes the entire video sequence and tracks the motion of selected points.
+        Detects and tracks radius for the main collision circle using classical techniques.
         """
-        # Input validation: ensure points have been marked for tracking
-        if not self.processor.points_to_track:
-            messagebox.showerror("Error", "Please mark points to track first.")
-            return
 
-        self.collision.track(self.processor.points_to_track)
+        self.popup = SpinnerPopup(self.videoview, self.cwidth, self.cheight)
 
-        self.track_coords_button.configure(state=ctk.NORMAL)  # Enable coordinates button
+        def trackbg(popup):
+            startidx = self.seekbar.startidx
+            endidx = self.seekbar.endidx
+            self.collision.track(self._rects, startidx, endidx)
+            
+            self.root.after(0, popup.destroy())
+
+            self.load_video(self._trackpath)
+
+            self.track_coords_button.configure(state=ctk.NORMAL)  # Enable coordinates button
+
+        threading.Thread(target=trackbg, args=(self.popup,)).start()
