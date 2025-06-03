9e55149506f9bd97a42e51ea4c80ddf4a5c6b899
Isam Balghari <isamabdullah88@gmail.com>
2025-06-03 09:30:10 +0500

    cleanup

--- gui/Welcome.py
+++ gui/Welcome.py
@@ -3,40 +3,39 @@ import customtkinter as ctk
 from PIL import ImageTk, Image
 
 class WelcomeScreen:
-    def __init__(self, master):
-        self.master = master
-        self.master.title("Welcome")
-        self.master.geometry("960x640")
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Welcome")
+        self.root.geometry("960x640")
 
         # Title label
-        self.label = ctk.CTkLabel(master, text="Welcome to PhysTrackX", font=("Helvetica", 24))
+        self.label = ctk.CTkLabel(root, text="Welcome to PhysTrackX", font=("Helvetica", 24))
         self.label.pack(pady=(20, 0))
 
         # Subtitle label
-        subtitle_label = ctk.CTkLabel(master, text="A Project By", font=("Helvetica", 18))
-        subtitle_label.pack(pady=(10, 0))
+        subtlabel = ctk.CTkLabel(root, text="A Project By", font=("Helvetica", 18))
+        subtlabel.pack(pady=(10, 0))
 
         # Load and display the image
-        self.load_and_display_image(master)
+        self.displogo(root)
 
         self.animate_text()
 
-    def load_and_display_image(self, master):
+    def displogo(self, root):
         # Load the image
-        image_path = "assets/physlab_logo.png"
-        image = Image.open(image_path)
-        photo = ImageTk.PhotoImage(image)
+        img = Image.open("assets/logo.png")
+        photo = ImageTk.PhotoImage(img)
 
         # Create a label to display the image
-        image_label = ctk.CTkLabel(master, image=photo)
-        image_label.image = photo  # Keep a reference, prevent GC
-        image_label.pack(pady=(10, 20))
+        imglabel = ctk.CTkLabel(root, image=photo)
+        imglabel.image = photo  # Keep a reference, prevent GC
+        imglabel.pack(pady=(10, 20))
 
     def animate_text(self):
         text = self.label.cget("text")
         if text.endswith("..."):
-            self.label.config(text="Welcome to PhysTrackX")
+            self.label.configure(text="Welcome to PhysTrackX")
         else:
-            self.label.config(text=text + ".")
-        self.master.after(500, self.animate_text)
+            self.label.configure(text=text + ".")
+        self.root.after(500, self.animate_text)
 
