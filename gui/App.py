b0616f140a72e6608adaae2fd6026d17c16f0fd2
Isam Balghari <isamabdullah88@gmail.com>
2025-06-11 01:05:51 +0500

    cleanup-2.

--- gui/App.py
+++ gui/App.py
@@ -62,16 +62,6 @@ class App:
             
         scroll_toolbar.pack()
         
-        # temp1 = ctk.CTkFrame(self.root, width=100, bg_color="#899fbd", fg_color="#e75ad0")
-        # temp1.pack_propagate(False)
-        # temp1.pack(side=ctk.LEFT)
-
-        # temp2 = ctk.CTkCanvas(temp1, width=100)
-        # temp2.pack(side=ctk.LEFT)
-        
-        # temp3 = ctk.CTkCanvas(temp2, width=100)
-        # temp3.pack(side=ctk.LEFT)
-        
         self.vidframe = ctk.CTkFrame(self.root, width=self.cwidth-self.twidth, height=self.theight, bg_color="#899fbd", fg_color="#5bdada")
         self.vidframe.pack_propagate(False)
         self.vidframe.pack(side=ctk.LEFT)
@@ -80,33 +70,6 @@ class App:
         self.vheight = self.theight-self.seekbarh-2*self.pady
         self.videoview = ctk.CTkCanvas(self.vidframe, width=self.vwidth, height=self.vheight, bg="silver") #, highlightbackground="black")
         self.videoview.pack(side=ctk.TOP, expand=False)
-        
-        # self.seekbar = CutSeekBar(self.vidframe, width=self.cwidth-self.twidth, height=self.seekbarh, bg="silver") #, highlightbackground="black")
-        
-        # Bind updates to scrollbar & resizing
-        # self.toolbarf.bind("<Configure>", self.onfconfigure)
-        # tcanvas.bind("<Configure>", self.oncconfigure)
-
-        # # Enable mousewheel scrolling
-        # self._tcanvas.bind_all("<MouseWheel>", self.onmwheel)  # Windows/macOS
-        # self._tcanvas.bind_all("<Button-4>", self.onmwheel)    # Linux scroll up
-        # self._tcanvas.bind_all("<Button-5>", self.onmwheel)    # Linux scroll down
-        
-
-
-    # def onfconfigure(self, event):
-    #     self.canvas.configure(scrollregion=self.canvas.bbox("all"), width=self.twidth)
-
-    # def oncconfigure(self, event):
-    #     # Resize inner frame's width to match canvas width
-    #     cwidth = event.width
-    #     self.canvas.itemconfig(self.toolbarwin, width=self.twidth)
-
-    # def onmwheel(self, event):
-    #     if event.num == 5 or event.delta == -120:
-    #         self._tcanvas.yview_scroll(1, "units")
-    #     elif event.num == 4 or event.delta == 120:
-    #         self._tcanvas.yview_scroll(-1, "units")
 
 
     def openvideo(self):
@@ -117,15 +80,6 @@ class App:
             # self.fps_label.configure(text=f"FPS: {self.processor.fps}")
             self.load_video(videopath)
     
-    # def load_video(self, videopath):
-    #     pass
-
-    # def dispframe(self, frame=None):
-    #     pass
-
-    # def updateframe(self, event):
-    #     pass
-        
     
     # def mark_line(self, event):
     #     if len(self.processor.line_coords) < 2:
@@ -139,68 +93,9 @@ class App:
     #                 text=f"Reference Distance: {self.processor.ref_distance} units")
     
 
-    # def markaxes(self):
-    #     pass
+    def markaxes(self):
+        pass
 
-    # def mark_points_to_track(self):
-    #     self.processor.points_to_track = []
-    #     messagebox.showinfo("Instruction", "Please click points on the video to mark them for tracking.")
-    #     self.videoview.bind("<Button-1>", self.mark_point)
-    
-    # def mark_point(self, event):
-    #     self.processor.points_to_track.append((event.x, event.y))
-    #     self.videoview.create_oval(event.x-3, event.y-3, event.x+3, event.y+3, fill="red")
-
-    # def choose_tracking_method(self):
-    #     self.tracking_method = ctk.StringVar()
-    #     self.tracking_method.set("points")  # default value
-
-    #     method_popup = ctk.CTkToplevel(self.root)
-    #     method_popup.title("Select Tracking Method")
-
-    #     ctk.CTkLabel(method_popup, text="Choose tracking method:").pack(pady=10)
-    #     ctk.CTkRadioButton(method_popup, text="Track Points", variable=self.tracking_method, value="points").pack(anchor=ctk.W)
-    #     ctk.CTkRadioButton(method_popup, text="Track Centroid", variable=self.tracking_method, value="bbox").pack(anchor=ctk.W)
-
-    #     ctk.CTkButton(method_popup, text="OK", command=lambda: self.confirm_tracking_method(method_popup)).pack(pady=10)
-
-    # def confirm_tracking_method(self, popup):
-    #     method = self.tracking_method.get()
-    #     popup.destroy()
-
-    #     if method == "points":
-    #         self.mark_points_to_track()
-    #     elif method == "bbox":
-    #         self.mark_bboxes_to_track()
-    #     else:
-    #         messagebox.showerror("Error", "Invalid method. Please select 'points' or 'bbox'.")
-
-    # def mark_bboxes_to_track(self):
-    #     # self.bboxes_to_track = []
-    #     messagebox.showinfo("Instruction", "Please draw bounding boxes around the objects to track. Draw multiple bounding boxes if needed.")
-    #     self.videoview.bind("<Button-1>", self.start_bbox)
-
-    # def start_bbox(self, event):
-    #     self.start_x, self.start_y = event.x, event.y
-    #     self.current_bbox = self.videoview.create_rectangle(self.start_x, self.start_y, event.x, event.y, outline="red")
-    #     self.videoview.bind("<B1-Motion>", self.draw_bbox)
-    #     self.videoview.bind("<ButtonRelease-1>", self.finish_bbox)
-
-    # def draw_bbox(self, event):
-    #     self.videoview.coords(self.current_bbox, self.start_x, self.start_y, event.x, event.y)
-
-    # def finish_bbox(self, event):
-    #     self.videoview.unbind("<B1-Motion>")
-    #     self.videoview.unbind("<ButtonRelease-1>")
-    #     bbox_coords = (self.start_x, self.start_y, event.x, event.y)
-    #     self.bboxes_to_track.append(bbox_coords)
-
-    #     frame_ox, frame_oy = self._ref_frame
-
-    #     centroid_x = (bbox_coords[0] + bbox_coords[2]) / 2 - frame_ox
-    #     centroid_y = (bbox_coords[1] + bbox_coords[3]) / 2 - frame_oy
-    #     self.processor.points_to_track.append((centroid_x, centroid_y))
-    #     self.videoview.create_oval(centroid_x - 3, centroid_y - 3, centroid_x + 3, centroid_y + 3, fill="red")
 
     def resizeframe(self, frame, fwidth, fheight):
         if (fwidth > self.vwidth):
