3ac3aa014fcd8e9c94b5bea17943eee89a4223d9
Isam Balghari <isamabdullah88@gmail.com>
2025-07-06 03:24:44 +0500

    clean text in OCR.

--- experiments/components/OCR.py
+++ experiments/components/OCR.py
@@ -1,4 +1,4 @@
-
+import re
 
 class OCRData:
     def __init__(self, data):
@@ -9,6 +9,18 @@ class OCRData:
         if self.datanum > 0:
             self.samplecount = len(self.data[0])
             
+        self.clean()
             
+    
+    def clean(self):
+        cdata = [[] for d in self.data]
+        
+        for i,text in enumerate(self.data):
+            for t in text:
+                tclean = re.sub(r"[^\d.]", "", t)
+                
+                cdata[i].append(tclean)
+                
+        self.data = cdata
     
     
