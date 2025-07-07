ee704ada0e6d9f7690eece3c95bd4a23268f89cd
Isam Balghari <isamabdullah88@gmail.com>
2025-07-07 13:16:12 +0500

    Styled label for frame count and rect coordindates.

---
+++ gui/components/Label.py
@@ -0,0 +1,20 @@
+
+import customtkinter as ctk
+
+class Label(ctk.CTkFrame):
+    def __init__(self, parent, text="Label", color="blue", **kwargs):
+        super().__init__(parent, fg_color="transparent", **kwargs)
+
+        # Colored box
+        self.colorbox = ctk.CTkLabel(self, text="", width=15, height=15, fg_color=color, corner_radius=3)
+        self.colorbox.pack(side="left", padx=(0, 8))
+
+        # Text label
+        self.label = ctk.CTkLabel(self, text=text, font=("Segoe UI", 16))
+        self.label.pack(side="left")
+
+    def set_text(self, text):
+        self.label.configure(text=text)
+
+    def set_color(self, color):
+        self.colorbox.configure(fg_color=color)
