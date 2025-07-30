0360e20c51e1062ce1aa1659779102e4ffba202a
Isam Balghari <isamabdullah88@gmail.com>
2025-07-30 12:01:41 +0500

    Integrate datamanager in plot and save. Add options to save. Refactor.

--- gui/components/plot/plot.py
+++ gui/components/plot/plot.py
@@ -1,285 +1,178 @@
+"""
+plot.py
+
+Responsible for plotting position and motion derivatives from processed data using matplotlib.
+
+Author: Isam Balghari
+"""
 
 import matplotlib.pyplot as plt
 import numpy as np
-from gui.components.axes import Axes
-from gui.components.checkbox import Checkbox
-from .plottype import PlotType
 import customtkinter as ctk
 
+from gui.components.checkbox import Checkbox
+from .plottype import PlotType
+from .datamanager import DataManager
+
+
 class Plot:
-    def __init__(self, parent, data:list[float], axes:Axes , vwidth, vheight, fwidth, fheight, fps=24,
-                scale=1, theme='ggplot'):
+    def __init__(self, parent, datamanager: DataManager, theme: str = 'ggplot') -> None:
         """
-        data: list of numpy arrays. Each array should have shape N,2.
-        ox, oy: origin of user specified coordinate axes.
-        fwidth: Width of video frame.
-        fheight: Height of video frame.
-        vwidth: Width of videoview frame.
-        vheight: Height of videoview frame.
+        Initializes the Plot component for visualizing data.
+
+        Args:
+            parent: The parent GUI container.
+            datamanager (DataManager): The data manager with transformed coordinates.
+            theme (str): Matplotlib theme style.
         """
         self.parent = parent
-        self._data = data
-        self.datanum = len(data)
-        self.fps = fps
-        self._fwidth = fwidth
-        self._fheight = fheight
-        self._vwidth = vwidth
-        self._vheight = vheight
-        self._scale = scale
-        self.axes = axes
-        
-        self.samplecount = 0
-        if self.datanum > 0:
-            self.samplecount = self._data[0].shape[0]
-        self._t = np.linspace(0, self.samplecount/fps, self.samplecount)
+        self.datamanager = datamanager
+
         plt.style.use(theme)
-        
-        # Transform
-        self._datatr = []
-        for i in range(self.datanum):
-            data = self._data[i]
-            
-            datax, datay = self.transform(data[:,0], data[:,1], axes)
-            datatr = np.hstack((datax.reshape(-1,1), datay.reshape(-1,1)))
-            
-            self._datatr.append(datatr)
-            
         self.checkbox = Checkbox(self.parent, PlotType, self.showplots)
-            
-    def data(self):
-        """Returns processed data after transformation"""
-        return self._datatr
-        
-    def transform(self, x, y, axes: Axes):
-        """Applies transformation according to videoview, image frame and user specified frame"""
-        fvx = self._vwidth/2 - self._fwidth/2
-        fvy = self._vheight/2 - self._fheight/2
-        ox = self.axes.ox
-        oy = self.axes.oy
-        
-        # In canvas coordinates
-        x = x + fvx
-        y = y + fvy
-        
-        x, y = axes.canvas2reg(x, y, ox, oy)
-        
-        # Inverse rotate points
-        theta = -np.deg2rad(self.axes.theta.get())
-        x, y = axes.rotatez(x, y, theta)
-        
-        if self._scale is not None:
-            x = x * self._scale
-            y = y * self._scale
-        
-        return x, y
-    
-    def showplots(self, selected_plots):
-        """Displays plot of selected plots stored in selected_plots"""
-        for ptype in selected_plots:
-            if ptype == PlotType.X.name:
-                self.plotx()
-            elif ptype == PlotType.Y.name:
-                self.ploty()
-            elif ptype == PlotType.XY.name:    
-                self.plotxy()
-            elif ptype == PlotType.DX.name:
-                self.plotdx()
-            elif ptype == PlotType.DY.name:
-                self.plotdy()
-            elif ptype == PlotType.D2X.name:
-                self.plotd2x()
-            elif ptype == PlotType.D2Y.name:
-                self.plotd2y()
-                
+
+    @property
+    def points(self):
+        return self.datamanager.processed_points
+
+    @property
+    def timestamps(self):
+        return self.datamanager.timestamps
+
+    def showplots(self, selected_plots: list[str]) -> None:
+        """Displays all selected plots."""
+        plot_map = {
+            PlotType.X.name: self.plotx,
+            PlotType.Y.name: self.ploty,
+            PlotType.XY.name: self.plotxy,
+            PlotType.DX.name: self.plotdx,
+            PlotType.DY.name: self.plotdy,
+            PlotType.D2X.name: self.plotd2x,
+            PlotType.D2Y.name: self.plotd2y
+        }
+
+        for plot_type in selected_plots:
+            plot_func = plot_map.get(plot_type)
+            if plot_func:
+                plot_func()
+
         plt.show(block=False)
-        
+
     def plotx(self):
-        for d in self._datatr:
+        for tpts in self.points:
             plt.figure()
-            plt.title(r"$\mathbf{x}$")
-            plt.xlabel(r"$\mathbf{t}$")
-            plt.ylabel(r"$\mathbf{x}$")
-            plt.plot(self._t, d[:, 0], '-m')
-            
+            plt.title("X vs Time")
+            plt.xlabel("Time (s)")
+            plt.ylabel("X")
+            plt.plot(self.timestamps, tpts[:, 0], '-m')
+
     def ploty(self):
-        for d in self._datatr:
+        for tpts in self.points:
             plt.figure()
-            plt.title(r"$\mathbf{y}$")
-            plt.xlabel(r"$\mathbf{t}$")
-            plt.ylabel(r"$\mathbf{y}$")
-            plt.plot(self._t, d[:, 1], '-m')
-            
+            plt.title("Y vs Time")
+            plt.xlabel("Time (s)")
+            plt.ylabel("Y")
+            plt.plot(self.timestamps, tpts[:, 1], '-m')
+
     def plotxy(self):
-        for d in self._datatr:
+        for tpts in self.points:
             plt.figure()
-            plt.title(r"$\mathbf{x,y}$")
-            plt.xlabel(r"$\mathbf{x}$")
-            plt.ylabel(r"$\mathbf{y}$")
-            plt.plot(d[:,0], d[:, 1], '-c')
-            
+            plt.title("Y vs X")
+            plt.xlabel("X")
+            plt.ylabel("Y")
+            plt.plot(tpts[:, 0], tpts[:, 1], '-c')
+
     def plotdx(self):
-        for d in self._datatr:
-            dx_dt = np.gradient(d[:,0], self._t).reshape(-1, 1)
+        for tpts in self.points:
+            dx_dt = np.gradient(tpts[:, 0], self.timestamps)
             plt.figure()
-            plt.title(r"$\mathbf{dx}$")
-            plt.xlabel(r"$\mathbf{t}$")
-            plt.ylabel(r"$\mathbf{dx}$")
-            plt.plot(self._t, dx_dt, '-g')
-            
-    
+            plt.title("dx/dt")
+            plt.xlabel("Time (s)")
+            plt.ylabel("dx/dt")
+            plt.plot(self.timestamps, dx_dt, '-g')
+
     def plotdy(self):
-        for d in self._datatr:
-            dy_dt = np.gradient(d[:,1], self._t).reshape(-1, 1)
+        for tpts in self.points:
+            dy_dt = np.gradient(tpts[:, 1], self.timestamps)
             plt.figure()
-            plt.title(r"$\mathbf{dy}$")
-            plt.xlabel(r"$\mathbf{t}$")
-            plt.ylabel(r"$\mathbf{dy}$")
-            plt.plot(self._t, dy_dt, '-g')
-            
+            plt.title("dy/dt")
+            plt.xlabel("Time (s)")
+            plt.ylabel("dy/dt")
+            plt.plot(self.timestamps, dy_dt, '-g')
+
     def plotd2x(self):
-        for d in self._datatr:
-            dx_dt = np.gradient(d[:,0], self._t)
-            dx2_dt = np.gradient(dx_dt, self._t)
+        for tpts in self.points:
+            dx_dt = np.gradient(tpts[:, 0], self.timestamps)
+            d2x_dt2 = np.gradient(dx_dt, self.timestamps)
             plt.figure()
-            plt.title(r"$\mathbf{d^2x}$")
-            plt.xlabel(r"$\mathbf{t}$")
-            plt.ylabel(r"$\mathbf{d^2x}$")
-            plt.plot(self._t, dx2_dt, '-b')            
-    
+            plt.title("d²x/dt²")
+            plt.xlabel("Time (s)")
+            plt.ylabel("d²x/dt²")
+            plt.plot(self.timestamps, d2x_dt2, '-b')
+
     def plotd2y(self):
-        for d in self._datatr:
-            dy_dt = np.gradient(d[:,1], self._t)
-            dy2_dt = np.gradient(dy_dt, self._t)
+        for tpts in self.points:
+            dy_dt = np.gradient(tpts[:, 1], self.timestamps)
+            d2y_dt2 = np.gradient(dy_dt, self.timestamps)
             plt.figure()
-            plt.title(r"$\mathbf{d^2y}$")
-            plt.xlabel(r"$\mathbf{t}$")
-            plt.ylabel(r"$\mathbf{d^2y}$")
-            plt.plot(self._t, dy2_dt, '-b')
-        
-        
-    # def intgr(self):
-    #     dataintr = []
-        
-    #     for i in range(self.datanum):
-            
-    #         x_intg = np.cumsum(self._datatr[i][:,0]).reshape(-1,1) * (self._t[1] - self._t[0])
-    #         y_intg = np.cumsum(self._datatr[i][:,1]).reshape(-1,1) * (self._t[1] - self._t[0])
-            
-    #         dataintr.append(np.hstack((x_intg,y_intg)).reshape(-1, 2))
-        
-    #     self.plot(dataintr, title="Integral")
-    
-    # def plotdrv(self):
-    #     datadrv = []
-        
-    #     for i in range(self.datanum):
-    #         dx_dt = np.gradient(self._datatr[i][:,0], self._t).reshape(-1, 1)
-    #         dy_dt = np.gradient(self._datatr[i][:,1], self._t).reshape(-1, 1)
-    #         datadrv.append(np.hstack((dx_dt,dy_dt)).reshape(-1, 2))
-        
-    #     self.plot(datadrv, title="1st Derivative")
-        
-    # def plotdrv2(self):
-    #     datadrv = []
-        
-    #     for i in range(self.datanum):
-    #         dx_dt = np.gradient(self._datatr[i][:,0], self._t)
-    #         dx2_dt = np.gradient(dx_dt, self._t)
-            
-    #         dy_dt = np.gradient(self._datatr[i][:,1], self._t)
-    #         dy2_dt = np.gradient(dy_dt, self._t)
-            
-    #         datadrv.append(np.vstack((dx2_dt,dy2_dt)).reshape(-1, 2))
-        
-    #     self.plot(datadrv, title="2nd Derivative")
-        
-    # def plot(self, trackpts, title="Data"):
-
-    #     _, axes = plt.subplots(self.datanum, 3, figsize=(10, 5), squeeze=False)
-
-    #     for i in range(self.datanum):
-    #         trackpt = trackpts[i]
-            
-    #         xcoords = trackpt[:, 0]
-    #         ycoords = trackpt[:, 1]
-
-    #         axes[i][0].plot(self._t, xcoords, '-m')
-    #         axes[i][0].set_xlabel(r"$\mathbf{t}$")
-    #         axes[i][0].set_ylabel(r"$\mathbf{x}$")
-    #         axes[i][0].set_title(title + r" - $\mathbf{x}$")
-    #         axes[i][0].set_aspect('equal')
-            
-    #         axes[i][1].plot(self._t, ycoords, '-g')
-    #         axes[i][1].set_xlabel(r"$\mathbf{t}$")
-    #         axes[i][1].set_ylabel(r"$\mathbf{y}$")
-    #         axes[i][1].set_title(title + r" - $\mathbf{y}$")
-    #         axes[i][1].set_aspect('equal')
-            
-    #         axes[i][2].plot(xcoords, ycoords, '-r')
-    #         axes[i][2].set_xlabel(r"$\mathbf{x}$")
-    #         axes[i][2].set_ylabel(r"$\mathbf{y}$")
-    #         axes[i][2].set_title(title + r" - $\mathbf{x}$ vs $\mathbf{y}$")
-    #         axes[i][2].set_aspect('equal')
-            
-    #     plt.tight_layout()
-    #     # plt.show()
-        
-    # def show(self):
-    #     plt.show()
-        
+            plt.title("d²y/dt²")
+            plt.xlabel("Time (s)")
+            plt.ylabel("d²y/dt²")
+            plt.plot(self.timestamps, d2y_dt2, '-b')
 
 
 def main():
     """
-    GUI-based test for Plot and Axes with dummy circular motion data.
-    Allows axes selection and plot type selection through checkboxes.
+    GUI-based test for the Plot system using dummy circular motion data.
     """
+    import numpy as np
+    import customtkinter as ctk
+    from gui.components.tpoints import FPoint
+    from gui.components.axes import Axes
+    from gui.components.plot.datamanager import DataManager
+    from gui.components.plot.plot import Plot
 
-    # Initialize GUI
-    ctk.set_appearance_mode("system")
+    # --- Setup GUI ---
+    ctk.set_appearance_mode("System")
     root = ctk.CTk()
     root.geometry("800x600")
     root.title("Plot and Axes Test")
 
-    # Create canvas
     canvas = ctk.CTkCanvas(root, width=640, height=480, bg="white")
     canvas.pack(pady=10)
 
-    # Dummy buttons just to satisfy Axes dependency
     btn_frame = ctk.CTkFrame(root)
     btn_frame.pack(pady=5)
-    dummy_btn = ctk.CTkButton(btn_frame, text="Axes")  # This is the button used for axes
-    dummy_btn.pack()
+    axes_btn = ctk.CTkButton(btn_frame, text="Set Axes")
+    axes_btn.pack()
 
-    btnlist = {"axes": dummy_btn}
-    activebtn = dummy_btn
+    btnlist = {"axes": axes_btn}
+    axes = Axes(root, canvas, vwidth=640, vheight=480, btnlist=btnlist, activebtn=axes_btn)
 
-    # Create Axes object (user sets origin and rotation angle interactively)
-    axes = Axes(root, canvas, vwidth=640, vheight=480, btnlist=btnlist, activebtn=activebtn)
-
-    # Bind axes setup on button click
-    dummy_btn.configure(command=axes.markaxes)
-
-    # Generate sample circular data
+    # --- Generate dummy data ---
     t = np.linspace(0, 2 * np.pi, 150)
     x = 50 + 30 * np.cos(t)
     y = 50 + 30 * np.sin(t)
-    data = [np.column_stack((x, y))]
+    fpoints = [[FPoint([x[i], y[i]], 0, 0) for i in range(len(x))]]
 
-    # Create Plot object (plot types shown after axes are applied)
-    plot = Plot(
-        parent=root,
-        data=data,
+    # --- Create DataManager ---
+    datamanager = DataManager(
+        tpoints=fpoints,
         axes=axes,
         vwidth=640,
         vheight=480,
         fwidth=640,
         fheight=480,
         fps=30,
-        scale=1.0,
-        theme='ggplot'
+        scale=1.0
     )
 
+    # --- Setup Axes and Plot after marking ---
+    def on_axes_applied():
+        datamanager.transform()
+        Plot(parent=root, datamanager=datamanager, theme='ggplot')
+
+    axes_btn.configure(command=lambda: [axes.markaxes(), root.after(3000, on_axes_applied)])
     root.mainloop()
 
 
--- gui/components/plot/save.py
+++ gui/components/plot/save.py
@@ -1,79 +1,165 @@
+"""
+save.py
+
+Handles saving transformed coordinate and OCR data to CSV using user-defined options.
+
+Author: Isam Balghari
+"""
 
 import csv
 import customtkinter as ctk
-from gui.components.plot.plot import Plot
-from experiments.components.ocr import OCRData
+from gui.components.plot.datamanager import DataManager
 from ..checkbox import Checkbox
 from .savetype import SaveType
 
+
 class Save:
-    def __init__(self, parent, pdata:Plot, ocrdata:OCRData):
+    def __init__(self, parent, datamanager: DataManager) -> None:
+        """
+        Initializes the Save handler for exporting processed data.
+
+        Args:
+            parent: The parent GUI container.
+            datamanager (DataManager): DataManager instance with processed tracking and OCR data.
+        """
         self.parent = parent
-        self.pdata = pdata
-        self.ocrdata = ocrdata
+        self.datamanager = datamanager
         self.filepath = None
-        
-        self.samplecount = max(self.pdata.samplecount, self.ocrdata.samplecount)
-        
+
         self.checkbox = Checkbox(self.parent, SaveType, self.savedata)
-        
-    
-    def askfilepath(self):
-        self.filepath = ctk.filedialog.asksaveasfilename(defaultextension=".csv", 
-                            filetypes=[("CSV files", "*.csv"), ("All files", "*.*")])
+
+    @property
+    def datacount(self) -> int:
+        return self.datamanager.datacount
+
+    @property
+    def samplecount(self) -> int:
+        return self.datamanager.samplecount
+
+    @property
+    def ocrcount(self) -> int:
+        return self.datamanager.ocrcount
+
+    @property
+    def points(self):
+        return self.datamanager.processed_points
+
+    @property
+    def ocr(self):
+        return self.datamanager.ocrdata
+
+    def askfilepath(self) -> None:
+        """Prompts user to select a file path for saving data."""
+        self.filepath = ctk.filedialog.asksaveasfilename(
+            defaultextension=".csv",
+            filetypes=[("CSV files", "*.csv"), ("All files", "*.*")]
+        )
+
+    def prepheader(self, savetypes: list[str]) -> list[str]:
+        """Prepares the CSV header based on selected save options."""
+        header = []
+        if SaveType.TIME.name in savetypes:
+            header.append("T(s)")
+
+        if SaveType.XY.name in savetypes:
+            for i in range(self.datacount):
+                header.extend([f"x{i+1}", f"y{i+1}"])
+
+        if SaveType.OCR.name in savetypes:
+            for i in range(self.ocrcount):
+                header.append(f"text{i+1}")
+
+        return header
+
+    def savedata(self, savetypes: list[str]) -> None:
+        """Saves selected data to CSV based on save type options."""
+        self.askfilepath()
         if not self.filepath:
             return
-        
-    def savedata(self, selected_data):
-        """Saves data onto file path"""
-        datalist = self.pdata.data()
-        with open(self.filepath, mode='w', newline='') as file:
-            
-            writer = csv.writer(file)
-            
-            dt = 1 / self.pdata.fps
-            ts = 0.0
-            
-            if SaveType.HEADER in selected_data:
-            # Prep header
-                header = ["T(s)"]
-                for i in range(self.pdata.datanum):
-                    header.extend([f"x{i+1}", f"y{i+1}"])
-                    
-                for i in range(self.ocrdata.datanum):
-                    header.extend([f"text{i+1}"])
 
-                writer.writerow(header) 
-            
-            # Persist data into file
+        with open(self.filepath, mode='w', newline='', encoding='utf-8') as file:
+            writer = csv.writer(file)
+
+            if SaveType.HEADER.name in savetypes:
+                writer.writerow(self.prepheader(savetypes))
+
             for i in range(self.samplecount):
-                row = [f"{ts:.06f}"]
-                
-                if i < self.pdata.samplecount:
-                    for tpoints in datalist:
-                        cx, cy = tpoints[i,:]
-                        row.extend([f"{cx:.02f}", f"{cy:.02f}"])
-                
-                if i < self.ocrdata.samplecount:
-                    for tocr in self.ocrdata.data:
-                        txt = tocr[i]
-                        row.extend([txt])
-                
-                ts += dt
+                row = []
+
+                if SaveType.TIME.name in savetypes:
+                    ts = self.datamanager.timestamps[i]
+                    row.append(f"{ts:.06f}")
+
+                if SaveType.XY.name in savetypes:
+                    for j in range(self.datacount):
+                        x, y = self.points[j][i, :]
+                        row.extend([f"{x:.02f}", f"{y:.02f}"])
+
+                if SaveType.OCR.name in savetypes:
+                    for o in self.ocr:
+                        row.append(o[i])
+
                 writer.writerow(row)
-                    
-                    
-                    
-if __name__ == "__main__":
+
+
+def main():
+    """
+    GUI-based test for Save functionality using DataManager and dummy data.
+    """
     import numpy as np
-    from .Axes import Axes
-    data = [np.random.rand(100, 2) * 100, np.random.rand(100, 2) * 100]
-    ocr = [["abc" for _ in range(100)]]
-    axes = Axes(ctk.CTk(), None, 200, 200)
-    plot = Plot(data, axes, 200, 200, 200, 200)
-    
-    save = Save(plot, ocr)
-    # save.askfilepath()
-    save.filepath = "test.csv"
-    save.savedata()
-    
+    import customtkinter as ctk
+    from gui.components.axes import Axes
+    from gui.components.tpoints import FPoint
+    from gui.components.plot.datamanager import DataManager
+    from experiments.components.ocr import OCRData
+    from gui.components.plot.save import Save
+    from gui.components.plot.savetype import SaveType
+
+    # Initialize GUI
+    ctk.set_appearance_mode("dark")
+    ctk.set_default_color_theme("blue")
+
+    root = ctk.CTk()
+    root.title("Save Data Test")
+    root.geometry("800x600")
+
+    # Dummy canvas and button for Axes object
+    canvas = ctk.CTkCanvas(root, width=640, height=480)
+    canvas.pack()
+    dummy_btn = ctk.CTkButton(root, text="Dummy")
+    btnlist = {"dummy": dummy_btn}
+    axes = Axes(root, canvas, vwidth=640, vheight=480, btnlist=btnlist, activebtn=dummy_btn)
+
+    # --- Dummy FPoint data ---
+    frame_count = 100
+    t = np.linspace(0, 2 * np.pi, frame_count)
+    x = 100 + 50 * np.cos(t)
+    y = 100 + 50 * np.sin(t)
+    fpoints = [[FPoint([x[i], y[i]], 0, 0) for i in range(frame_count)]]
+
+    # --- Dummy OCR data ---
+    ocr_text = [["OCR={:.2f}s".format(i / 24) for i in range(frame_count)]]
+    ocrdata = OCRData(ocr_text)
+
+    # --- DataManager ---
+    datamanager = DataManager(
+        tpoints=fpoints,
+        ocrdata=ocrdata,
+        axes=axes,
+        vwidth=640,
+        vheight=480,
+        fwidth=640,
+        fheight=480,
+        fps=24,
+        scale=1.0
+    )
+    datamanager.transform()
+
+    # --- Save handler ---
+    saver = Save(root, datamanager=datamanager)
+
+    root.mainloop()
+
+
+if __name__ == "__main__":
+    main()
