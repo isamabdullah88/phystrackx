ad74d45ec8e8e503ae3a42ea35c1a24efb8ae27e
Isam Balghari <isamabdullah88@gmail.com>
2025-08-12 18:02:46 +0500

    Small cleanup.

--- gui/rigid/rigidapp.py
+++ gui/rigid/rigidapp.py
@@ -72,7 +72,7 @@ class RigidApp(App):
         self.plot = None
         self.datamanager = None
 
-    def loadvideo(self, videopath: str, clear=True):
+    def loadvideo(self, videopath: str):
         """Loads the video into the viewer and initializes related components."""
         self.title = TitleBar(self.videoview, self.vwidth, "Video View")
         self.spinner = Spinner(self.videoview, self.videoapp.imgview)
