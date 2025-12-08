51a5ac60da5773dac931ba28b02016b073247d0d
Isam Balghari <isamabdullah88@gmail.com>
2025-12-08 12:57:43 +0500

    cleanup.

--- gui/app.py
+++ gui/app.py
@@ -71,9 +71,9 @@ class App:
 
     def toolbar(self) -> None:
         """Constructs toolbar and video area layout."""
-        self.scrollframe = ctk.CTkScrollableFrame(self.root, width=self.twidth-2.7*self.padx, height=self.theight,
+        self.scrollframe = ctk.CTkScrollableFrame(self.root, width=self.twidth-self.padx, height=self.theight,
                                                   bg_color="#899fbd", fg_color="#5bdada")
-        self.scrollframe.pack(padx=0, pady=0, side=ctk.LEFT, fill=ctk.X)
+        self.scrollframe.pack(padx=0, pady=0, side=ctk.LEFT)
         self.root.update_idletasks()
         print('scroll frame width: ', self.scrollframe.winfo_width())
 
@@ -98,11 +98,12 @@ class App:
         self.vidframe = ctk.CTkFrame(self.root, width=self.vwidth, height=self.theight,
                                      bg_color="#899fbd", fg_color="#5bdada")
         # self.vidframe.pack_propagate(False)
-        self.vidframe.pack(side=ctk.LEFT, expand=True, fill="both")
+        self.vidframe.pack(side=ctk.LEFT, expand=True, fill="both", padx=0)
 
         self.videoview = ctk.CTkCanvas(self.vidframe, width=self.vwidth, height=self.vheight, bg="#4d535c")
         # self.videoview.pack_propagate(False)
-        self.videoview.pack(side=ctk.TOP, expand=True, fill=ctk.BOTH)
+        # self.videoview.pack(side=ctk.TOP, expand=True, fill=ctk.BOTH, padx=0)
+        self.videoview.pack(fill=ctk.BOTH, expand=True)
 
         # Title and Axes setup
         self.title = TitleBar(self.videoview, self.vwidth, "Welcome!")
